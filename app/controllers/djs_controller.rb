e1a6c081f2b2e50045778d6455f2655a1dba1e09
Shiva Muraleedaran <shivamuraleedaran@hotmail.com>
2023-07-27 21:28:06 +1000

    djs_controller and routes and views files started and seeds complete

---
+++ app/controllers/djs_controller.rb
@@ -0,0 +1,71 @@
+class DjsController < ApplicationController
+
+  def index
+
+    @djs = Dj.all
+
+  end
+
+  def new
+
+    @dj = Dj.new
+
+  end
+
+  def show
+    @dj = Dj.find(params[:id])
+
+  end
+
+
+def create
+     @dj = Dj.new(dj_params)
+    @dj.user = current_user
+  if @dj.save
+
+    redirect_to dj_path(@dj)
+
+  else
+    render :new, status: :unprocessable_entity
+  end
+
+end
+
+def edit
+  @dj = Dj.find(params[:id])
+  render :new
+end
+
+def update
+  @dj = Dj.find(params[:id])
+
+  if @dj.update(dj_params)
+    redirect_to dj_path(@dj), notice: "DJ Listing updated successfully."
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
+
+def destroy
+  @dj = Dj.find(params[:id])
+
+  if @dj.destroy
+    redirect_to root_path, notice: "DJ Listing deleted successfully."
+  else
+    redirect_to dj_path(@dj), alert: "Failed to delete DJ Listing."
+  end
+end
+
+
+
+#
+
+private
+
+  def dj_params
+    params.require(:dj).permit(:name, :genre_id, :rate, :description, :photo)
+  end
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,104 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Create genres
+genres = [
+  "House",
+  "Techno",
+  "Electronic",
+  "Hip-hop",
+  "Trance",
+  "Drum and Bass",
+  "Dubstep",
+  "Tech House",
+  "Deep House",
+  "Progressive House",
+  "Disco",
+  "Funk",
+  "Soul",
+  "Trap",
+  "R&B",
+  "Reggae",
+  "Dancehall",
+  "Salsa",
+  "Bachata",
+  "Merengue",
+  "Cumbia",
+  "Reggaeton",
+  "Moombahton",
+  "Latin",
+  "Afrobeat",
+  "Kizomba",
+  "Soca",
+  "Bollywood",
+  "Bhangra",
+  "Jazz",
+  "Funk",
+  "Soul",
+  "Pop",
+  "Rock",
+  "Indie",
+  "Alternative",
+  "Metal",
+  "Punk",
+  "Classical",
+  "Blues",
+  "Ambient",
+  "Chillout",
+  "Lounge",
+  "Nu-Disco",
+  "Electro",
+  "Hardstyle",
+  "Tropical House",
+  "Melodic Techno",
+  "Tech Trance",
+  "Psytrance",
+  "Hardcore",
+  "Eurodance",
+  "Future Bass"
+]
+
+genres.each do |category|
+  Genre.create!(category: category)
+end
+
+
+
+
+#Create DJ
+5.times do
+  rate = rand(3..5)
+  description = "Sample DJ description."
+  genre_id = rand(1..5)
+  user_id = rand(1..5)
+  name = "Sample DJ #{rand(100)}"
+  photo = "sample_photo.jpg"
+
+  Dj.create!(
+  rate: rate,
+  description: description,
+  genre_id: genre_id,
+  user_id: user_id,
+  name: name,
+  photo: photo
+  )
+  end
+
+  # Create bookings
+  5.times do
+  start_date = Date.today + rand(1..30).days
+  end_date = start_date + rand(1..5).days
+  total_price = rand(50.0..300.0)
+  dj_id = rand(1..5)
+  user_id = rand(1..5)
+
+  Booking.create!(
+  start_date: start_date,
+  end_date: end_date,
+  total_price: total_price,
+  dj_id: dj_id,
+  user_id: user_id
+  )
+end
