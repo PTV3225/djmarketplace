5795ce6a05353657caa06d0bb5630d3ebd548821
Peter Varley <petertvarley@gmail.com>
2023-08-03 19:46:56 +1000

    deleted flatpickr in JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,13 +2,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-import "flatpickr-rails"
+// import "flatpickr-rails"
 
 
 
-document.addEventListener('turbo:load', function() {
-  flatpickr('.flatpickr', {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i",
-  });
-});
+// document.addEventListener('turbo:load', function() {
+//   flatpickr('.flatpickr', {
+//     enableTime: true,
+//     dateFormat: "Y-m-d H:i",
+//   });
+// });
